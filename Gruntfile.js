2f7b58c8c6749cfc29fa2cad2721296f620807fb
dnzk <danishkalesaran@gmail.com>
2016-05-15 23:32:05 +0700

    create initial tasks for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,13 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    less: {
+      develop: {
+        files: {
+          'build/app.css': 'src/app.less'
+        }
+      }
+    },
     pug: {
       compile: {
         options: {
@@ -10,12 +17,24 @@ module.exports = function(grunt) {
           'build/index.html': 'src/index.pug',
         }
       }
-    }
+    },
+    watch: {
+      less: {
+        files: ['src/**/*.less'],
+        tasks: ['less:develop']
+      },
+      pug: {
+        files: ['src/**/*.pug'],
+        tasks: ['pug:compile']
+      }
+    },
   });
 
-  var npmTasks = ['grunt-contrib-pug'];
+  var npmTasks = ['grunt-contrib-pug', 'grunt-contrib-less', 'grunt-contrib-watch'];
   npmTasks.forEach(function(npmTask) {
     grunt.loadNpmTasks(npmTask);
   });
+
+  grunt.registerTask('dev', ['pug:compile', 'less:develop', 'watch']);
   
 };
