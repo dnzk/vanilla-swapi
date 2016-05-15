74884c6897c1d1d031a53bcf1163e5be92a118b0
dnzk <danishkalesaran@gmail.com>
2016-05-15 17:07:35 +0700

    compile pug task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,21 @@
 module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pug: {
+      compile: {
+        options: {
+          pretty: true,
+        },
+        files: {
+          'build/index.html': 'src/index.pug',
+        }
+      }
+    }
+  });
+
+  var npmTasks = ['grunt-contrib-pug'];
+  npmTasks.forEach(function(npmTask) {
+    grunt.loadNpmTasks(npmTask);
+  });
   
 };
