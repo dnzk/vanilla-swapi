87a45f3db98c52b7d5ee7f2a4e88d5bb63d63701
dnzk <danishkalesaran@gmail.com>
2016-05-17 02:27:02 +0700

    clean up model.js

--- src/assets/script/model.js
+++ src/assets/script/model.js
@@ -2,23 +2,6 @@ var Model = (function(window) {
 
   var base = 'http://swapi.co/api/';
 
-  // function request(type, resource, success, error) {
-  //   var xhr = new XMLHttpRequest();
-
-  //   xhr.onreadystatechange = function() {
-  //     if (xhr.readyState === 4) {
-  //       if (xhr.status >= 200 && xhr.status < 400) {
-  //         success(JSON.parse(xhr.response));
-  //       } else {
-  //         error(JSON.parse(xhr.response));
-  //       }
-  //     }
-  //   };
-
-  //   xhr.open(type, base + resource, true);
-  //   xhr.send();
-  // }
-
   function ModelClass(resource) {
     if (resource.indexOf('#') === 0) {
       this.resource = resource.replace('#', '');
@@ -28,7 +11,6 @@ var Model = (function(window) {
   }
 
   ModelClass.prototype.get = function(success, error) {
-    // request('GET', this.resource, success, error);
     success = success || function() {};
     error = error || function() {};
 
