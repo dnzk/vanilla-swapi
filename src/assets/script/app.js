5fa028f3d8f142b246b16a9bd51aa4e2452d501d
dnzk <danishkalesaran@gmail.com>
2016-05-16 18:35:15 +0700

    separate model to its own file

--- src/assets/script/app.js
+++ src/assets/script/app.js
@@ -1,36 +1,3 @@
-var Model = (function(window) {
-
-  var base = 'http://swapi.co/api/';
-
-  function request(type, resource, success, error) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status >= 200 && xhr.status < 400) {
-          success(JSON.parse(xhr.response));
-        } else {
-          error(JSON.parse(xhr.response));
-        }
-      }
-    };
-
-    xhr.open(type, base + resource, true);
-    xhr.send();
-  }
-
-  function ModelClass(resource) {
-    this.resource = resource + '/';
-  }
-
-  ModelClass.prototype.get = function(success, error) {
-    request('GET', this.resource, success, error);
-  };
-
-  return ModelClass;
-
-}(window));
-
 var App = (function(document, window) {
 
   function bootstrap() {
