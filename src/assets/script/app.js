64ebac1a38d40ed8b8a7a66801cc475f14d0abc1
dnzk <danishkalesaran@gmail.com>
2016-05-17 00:22:24 +0700

    clean up app.js

--- src/assets/script/app.js
+++ src/assets/script/app.js
@@ -6,24 +6,6 @@ var App = (function(document, window) {
         return;
       }
       Router.goTo(window.location.hash);
-      // var people = new Model('people');
-      // // people.get(success, error);
-
-      // function success(data) {
-      //   // render
-      //   var results = data.results;
-
-      //   var feedListTemplate = document.querySelector('#template-feed-list');
-
-      //   results.forEach(function(result) {
-      //     var singleFeed = feedListTemplate.content.querySelector('.main-feed');
-      //     singleFeed.querySelector('.name').innerHTML = result.name;
-      //     var clone = document.importNode(singleFeed, true);
-      //     document.querySelector('.feed-container').appendChild(clone);
-
-      //   });
-
-      // }
 
       function error(data) {
         // handle error
